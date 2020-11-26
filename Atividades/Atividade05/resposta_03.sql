77365ee53a55f20d1273072dafbd1e7092544bfa
Rafiou Sitou <rafiousitou90@gmail.com>
2020-11-26 15:48:28 -0300

    feat(atividades): Add Atividade05 respostas.
    - Add resposta_01.sql
    - Add resposta_02.sql
    - Add resposta_03.sql

---
+++ Atividades/Atividade05/resposta_03.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `departamento` (
+    `codigo`          INT(11)      NOT NULL,
+    `nome`            VARCHAR(100) NOT NULL,
+    PRIMARY KEY (`codigo`),
+    UNIQUE KEY `codigo` (`codigo`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `funcionario` (
+    `matricula`       INT(11)      NOT NULL,
+    `nome`            VARCHAR(100) NOT NULL,
+    `salario`         DECIMAL(10,2) NOT NULL,
+    `gerente`         INT(11)      DEFAULT NULL,
+    `departamento`    INT(11)      NOT NULL,
+    PRIMARY KEY (`matricula`),
+    UNIQUE KEY `matricula` (`matricula`) USING HASH,
+    KEY `departamento` (`departamento`) USING BTREE,
+    KEY `funcionario_FK_1` (`gerente`),
+    CONSTRAINT `funcionario_FK` FOREIGN KEY (`departamento`) REFERENCES `departamento` (`codigo`),
+    CONSTRAINT `funcionario_FK_1` FOREIGN KEY (`gerente`) REFERENCES `funcionario` (`matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
